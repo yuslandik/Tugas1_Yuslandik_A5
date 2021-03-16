16090d43b995cef0f620041fb1d05a343a6ea128
yuslandik <61959296+yuslandik@users.noreply.github.com>
2021-03-16 22:32:03 +0800

    Add files via upload

---
+++ Incr.java
@@ -0,0 +1,16 @@
+/* Effek dari operator ++ */
+public class Incr {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+/* Kamus */
+int i, j;
+/* Program */
+i = 3;
+j = i++;
+System.out.println ("Nilai i : " + (++i) +
+"\nNilai j : " + j); 
+}
+}
